a31a69b137cfe1906ac1c9ff4ce6a5997a08321c
manu <manuelacostafaerman@gmail.com>
2024-07-22 15:11:34 +0200

    todo regreso

--- wedding_main/settings_production.py
+++ wedding_main/settings_production.py
@@ -1,6 +1,10 @@
 import dj_database_url
 from decouple import config
 import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -18,3 +22,14 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_ADDRESSING_STYLE = "virtual"
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, "static"),
+
+
+                    )
+STATIC_URL = AWS_URL + 'static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = AWS_URL + '/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
